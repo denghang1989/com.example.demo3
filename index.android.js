140a0d26c3b1f625e394656de81d20a29897aaf9
denghang1989 <denghang1989@163.com>
2019-09-12 09:11:34 +0800

    集成nativebase

--- index.android.js
+++ index.android.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 import { Text, AppRegistry ,View} from 'react-native';
-import {Button,Container} from 'native-base'
 
 export default class App extends Component {
   render() {
       return (
-              <Container>
-                  <Text>Hello world</Text>
-                  <Button light><Text>button</Text></Button>
-              </Container>
+              <Text>Hello world!</Text>
       );
   }}
 
